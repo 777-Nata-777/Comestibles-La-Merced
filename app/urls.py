c22012b7c96f3977cc89e6b673a18583148cbd25
Tejerina Jose Natanael <jtejerina51@gmail.com>
2025-05-26 02:08:16 -0300

    solucion de navegacion

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index,name='inicio'),
+    path('Iniciar Sesion', views.iniciar, name='Iniciar Sesion'),
+    path('Registrarse', views.registrarse, name='Registrarse'),
 ]
