8ee3ff1daf337c79af1f1421604ffeaebce2bde8
Tejerina Jose Natanael <jtejerina51@gmail.com>
2025-05-27 17:22:59 -0300

    CRUD terminado

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import producto
+from .forms import ProductoForm
 
 # Create your views here.
 def index(request):
@@ -10,10 +13,23 @@ def registrarse(request):
 def comestibles(request):
     return render(request, 'comestibles.html')
 def productos (request):
-    return render(request, 'Productos/index.html')
+    productos = producto.objects.all()
+    return render(request, 'Productos/index.html', {'productos': productos})
 def empieza (request):
     return render(request, 'empieza.html')
 def crear(request):
-    return render(request, 'Productos/crear.html')
-def editar(request):
-    return render(request, 'Productos/editar.html')
+    formulario = ProductoForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('productos')
+    return render(request, 'Productos/crear.html', {'formulario': formulario})
+def editar(request, id):
+    producto = productos.objects.get(id=id)
+    formulario = ProductoForm(request.POST or None, request.FILES or None, instace=producto)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+    return render(request, 'Productos/editar.html', {'formulario': formulario})
+def eliminar(request, id):
+    producto = productos.objects.get(id=id)
+    producto.delete()
+    return render(request, 'Productos')
